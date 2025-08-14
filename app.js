84d70ab7d2eee353d8357f2f5c422e34049df726
jasdeepsid <jasdeepsidhu647@gmail.com>
2025-08-14 11:06:34 -0600

    comment

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
 
 
-app.listen(PORT, () => {
-    console.log(`Connected to port ${PORT}`)
-});
 
+mongoose.connect(uri).then(
+async ()=> {
+
+    console.log('Connected to MongoDB Server');
+
+    app.listen(PORT, () => {
+        console.log(`Connected to port ${PORT}`)
+    });
+}).catch((err) =>{ console.log(`error: ${err}`)});
